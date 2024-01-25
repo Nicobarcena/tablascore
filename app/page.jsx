89eb28c41a17ce737c4555ea8ea21f41555d14a0
Nicobarcena <nicobarcena676@gmail.com>
2024-01-25 17:32:05 -0300

    limpieza

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      {/*page.js es la pagina del home */}
+      <h1>Hola</h1>
+      
+    </main>
+  );
+}
