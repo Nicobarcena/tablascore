a8c1b5aaf37ef9de1866eeef26d1c96668c5afd2
Nicobarcena <120732193+Nicobarcena@users.noreply.github.com>
2024-01-26 10:49:07 -0300

    ejemplohook

--- app/nosotros/Page.jsx
+++ app/nosotros/Page.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-
+"use client"
+import React, { useState } from 'react'
 const Nosotros = () => {
-  return (
-    <div>Estas en la pagina Nosotros</div>
-  )
+    
+    const [numero, setNumero] = useState(22)
+    
+    function sumar (){
+        const nuevoNumero = numero + 1
+        setNumero(nuevoNumero)
+    }
+    
+    return (
+    <>
+    <h1>Estas en la pagina Nosotros</h1>
+    <p>contador de cosas <br />
+    vamos por el numero: {numero} </p>
+    <button onClick={sumar}>sumar 1</button>
+    </>
+    )
 }
 
 export default Nosotros
