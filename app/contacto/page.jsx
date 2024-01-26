e0a62d0d4d5ac046c8fd9207537f2ee45da6fe6d
Nicobarcena <120732193+Nicobarcena@users.noreply.github.com>
2024-01-26 11:44:26 -0300

    fofofof

--- app/contacto/page.jsx
+++ app/contacto/page.jsx
@@ -4,29 +4,36 @@ const Contacto = () => {
   return (
     <>
     <h1>Iniciar Sesion</h1>
-    <form name="contacto" hidden data-netlify="true" netlify-honeypot="bot-field">
-        <input type="text" name="usuario" />
-        <input type="email" name="contraseña" />
-        <textarea name="comentarios"></textarea>
+    <form 
+                action="./contacto/gracias" 
+                name="contacto" 
+                hidden 
+                data-netlify="true" 
+                netlify-honeypot="bot-field">
+                <input type="text" name="nombre" />
+                <input type="email" name="email" />
+                <textarea name="comentarios"></textarea>
             </form>
-    
-    <form method='post' className="formulario">
-      <label>
-        Usuario:
-        <input type="text" name='usuario' />
-      </label>
-      <label>
-        Contraseña:
-        <input type="email" name='contraseña' />
-      </label>
-      <label>
+
+            <form method="post" className="formulario">
+                <label>
+                    Nombre:
+                    <input type="text" name="nombre" />
+                </label>
+                <label>
+                    E-mail:
+                    <input type="email" name="email" />
+                </label>
+                <label>
                     Comentarios:
                     <textarea name="comentarios"></textarea>
                 </label>
-      <input type="hidden" name='form-name' value="usuario" />
-      
-      <input type="submit" value="Ingresar" />
-    </form>
+
+                {/* Conectamos al formulario invisible */}
+                <input type="hidden" name="form-name" value="contacto" />
+
+                <input type="submit" value="Enviar" />
+            </form>
     
     </>
   )
