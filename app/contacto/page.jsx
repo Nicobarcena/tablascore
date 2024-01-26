f0858d2fb8827c049bb1b13938b23e543c819fa4
Nicobarcena <120732193+Nicobarcena@users.noreply.github.com>
2024-01-26 11:17:23 -0300

    form

--- app/contacto/page.jsx
+++ app/contacto/page.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
-
+import "./contacto.css"
 const Contacto = () => {
   return (
-    <div>page</div>
+    <>
+    <h1>Iniciar Sesion</h1>
+    <form method='post' className="formulario">
+      <label>
+        Usuario:
+        <input type="text" name='usuario' />
+      </label>
+      <label>
+        Contraseña:
+        <input type="password" name='contraseña' />
+      </label>
+      <input type="submit" value="Ingresar" />
+    </form>
+    
+    </>
   )
 }
 
